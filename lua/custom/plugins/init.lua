9e1cfd5a2681bfa249f74cf222a4931c67db963b
Mike Montano <edgesftw@gmail.com>
2024-12-09 11:57:51 -0600

    resync desktop and laptop

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -29,17 +29,6 @@ return {
     'shumphrey/fugitive-gitlab.vim',
   },
   {
-    'nvim-treesitter/nvim-treesitter',
-    config = function()
-      -- setup treesitter with config
-    end,
-    dependencies = {
-      -- NOTE: additional parser
-      { 'nushell/tree-sitter-nu', build = ':TSUpdate nu' },
-    },
-    build = ':TSUpdate',
-  },
-  {
     'epwalsh/obsidian.nvim',
     version = '*', -- recommended, use latest release instead of latest commit
     lazy = false,
@@ -72,6 +61,69 @@ return {
         {
           name = 'GradSU',
           path = '~/OneDrive/Obsidian/Grad-SU-Valt',
+        },
+      },
+      daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = 'dailies',
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        date_format = '%Y-%m-%d',
+        -- Optional, if you want to change the date format of the default alias of daily notes.
+        alias_format = '%B %-d, %Y',
+        -- Optional, default tags to add to each new daily note created.
+        default_tags = { 'daily-notes' },
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        template = nil,
+      },
+
+      completion = {
+        -- Set to false to disable completion.
+        nvim_cmp = true,
+        -- Trigger completion at 2 chars.
+        min_chars = 2,
+      },
+
+      -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
+      ui = {
+        enable = true, -- set to false to disable all additional syntax features
+        update_debounce = 200, -- update delay after a text change (in milliseconds)
+        max_file_length = 5000, -- disable UI features for files with more than this many lines
+        -- Define how various check-boxes are displayed
+        checkboxes = {
+          -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+          [' '] = { char = '󰄱', hl_group = 'ObsidianTodo' },
+          ['x'] = { char = '', hl_group = 'ObsidianDone' },
+          ['>'] = { char = '', hl_group = 'ObsidianRightArrow' },
+          ['~'] = { char = '󰰱', hl_group = 'ObsidianTilde' },
+          ['!'] = { char = '', hl_group = 'ObsidianImportant' },
+          -- Replace the above with this if you don't have a patched font:
+          -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+          -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+          -- You can also add more custom ones...
+        },
+        -- Use bullet marks for non-checkbox lists.
+        bullets = { char = '•', hl_group = 'ObsidianBullet' },
+        external_link_icon = { char = '', hl_group = 'ObsidianExtLinkIcon' },
+        -- Replace the above with this if you don't have a patched font:
+        -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+        reference_text = { hl_group = 'ObsidianRefText' },
+        highlight_text = { hl_group = 'ObsidianHighlightText' },
+        tags = { hl_group = 'ObsidianTag' },
+        block_ids = { hl_group = 'ObsidianBlockID' },
+        hl_groups = {
+          -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+          ObsidianTodo = { bold = true, fg = '#f78c6c' },
+          ObsidianDone = { bold = true, fg = '#89ddff' },
+          ObsidianRightArrow = { bold = true, fg = '#f78c6c' },
+          ObsidianTilde = { bold = true, fg = '#ff5370' },
+          ObsidianImportant = { bold = true, fg = '#d73128' },
+          ObsidianBullet = { bold = true, fg = '#89ddff' },
+          ObsidianRefText = { underline = true, fg = '#c792ea' },
+          ObsidianExtLinkIcon = { fg = '#c792ea' },
+          ObsidianTag = { italic = true, fg = '#89ddff' },
+          ObsidianBlockID = { italic = true, fg = '#89ddff' },
+          ObsidianHighlightText = { bg = '#75662e' },
         },
       },
 
@@ -120,6 +172,18 @@ return {
             env = {
               url = 'http://desktop-ak5t:11434',
             },
+            schema = {
+              model = {
+                -- default = 'llama3.2-vision:latest',
+                default = 'qwen2.5-coder:32b',
+              },
+              -- num_ctx = {
+              --   default = 16384,
+              -- },
+              -- num_predict = {
+              --   default = -1,
+              -- },
+            },
             parameters = {
               sync = true,
             },
@@ -134,4 +198,5 @@ return {
       vim.keymap.set('n', '<leader>aa', vim.cmd.CodeCompanionActions, { desc = '[A]rtificial Inteligence [A]ctions' })
     end,
   },
+  { 'github/copilot.vim' },
 }
